c143b677f22af02f93d36b7c3ce6ad3176bbc7fc
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-05-05 17:38:41 -0500

    organiza la funcion para factorizar un numero ingresado

--- ejercicios/factorizar.js
+++ ejercicios/factorizar.js
@@ -1,10 +1,17 @@
+// Funcion que factoriza un numero ingresado
+//
+//First Factorial
+//take the num parameter being passed and return the factorial of it.
+//For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24.
+
 function factorialize(num) {
   var resultado = num;
   if (num <= 0 || num === 1) return 1;
   while (num > 1) {
-    console.log(num);
     num--;
     resultado *= num;
   }
   return resultado;
 }
+
+console.log(factorialize(5));
