032affed3d56f06ebeb7599796f3c7b3f86c746e
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-05-05 17:33:06 -0500

    organiza la funcion de invertir palabras

--- ejercicios/invertirPalabras.js
+++ ejercicios/invertirPalabras.js
@@ -1,4 +1,10 @@
-function Reversa(str) {
+// Funcion que invierte las palabras ingresadas
+
+// First Reverse
+// take the str parameter being passed and return the string in reversed order.
+// For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+function reversa(str) {
   let texto = str;
   const enArray = texto.split("");
   const primera = enArray.reverse();
@@ -6,3 +12,5 @@ function Reversa(str) {
 
   return unirReversa;
 }
+
+console.log(reversa("Hello World and Coders"));
